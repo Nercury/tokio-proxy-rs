20226e540f19eb0adefe716feb19356327eea53c
Nerijus Arlauskas <nercury@gmail.com>
2016-11-04 20:55:01 +0200

    Remove unwraps and simplify code a bit.

--- src/error.rs
+++ src/error.rs
@@ -1,5 +1,23 @@
 use std::result;
+use std::net;
+use std::io;
 
 #[derive(Debug)]
-pub enum Error {}
+pub enum Error {
+    AddrParseError(net::AddrParseError),
+    Io(io::Error),
+}
+
+impl From<net::AddrParseError> for Error {
+    fn from(other: net::AddrParseError) -> Self {
+        Error::AddrParseError(other)
+    }
+}
+
+impl From<io::Error> for Error {
+    fn from(other: io::Error) -> Self {
+        Error::Io(other)
+    }
+}
+
 pub type Result<T> = result::Result<T, Error>;
--- src/server.rs
+++ src/server.rs
@@ -2,7 +2,6 @@ use error::Result;
 
 use std::net::SocketAddr;
 
-use futures;
 use futures::{Future};
 use futures::stream::Stream;
 use tokio_core::io::{copy, Io};
@@ -10,37 +9,32 @@ use tokio_core::net::{TcpListener, TcpStream};
 use tokio_core::reactor::Core;
 
 pub fn run(listen_port: String, forward_to: String) -> Result<()> {
-    let addr = format!("0.0.0.0:{}", listen_port)
-        .parse::<SocketAddr>().unwrap();
-
-    let fwd_addr = forward_to.parse::<SocketAddr>().unwrap();
+    let listen_addr = try!(format!("0.0.0.0:{}", listen_port).parse::<SocketAddr>());
+    let fwd_addr = try!(forward_to.parse::<SocketAddr>());
 
     // Create the event loop that will drive this server
-    let mut l = Core::new().unwrap();
+    let mut l = try!(Core::new());
     let handle = l.handle();
 
     // Create a TCP listener which will listen for incoming connections
-    let accept_socket = TcpListener::bind(&addr, &handle).unwrap();
+    let accept_socket = try!(TcpListener::bind(&listen_addr, &handle));
 
     let done = accept_socket.incoming().for_each(move |(server_socket, _addr)| {
         let client_pair = TcpStream::connect(&fwd_addr, &handle).map(|socket| socket.split());
 
-        let pair = futures::lazy(move || futures::finished(server_socket.split()));
-        let amt = pair.join(client_pair)
-            .and_then(move |((server_reader, server_writer), (client_reader, client_writer))| {
+        let amt = client_pair
+            .and_then(move |(client_reader, client_writer)| {
+                let (server_reader, server_writer) = server_socket.split();
                 let upload = copy(server_reader, client_writer);
                 let download = copy(client_reader, server_writer);
                 upload.join(download)
             });
 
         let msg = amt.then(|res| {
-            match res {
-                Ok((sent, received)) => {
-                    println!("bytes sent {:?}, bytes received {:?}", sent, received);
-                    return Ok(());
-                },
-                Err(_) => Ok(()),
+            if let Ok((sent, received)) = res {
+                println!("bytes sent {:?}, bytes received {:?}", sent, received);
             }
+            Ok(())
         });
 
         handle.spawn(msg);
@@ -48,7 +42,7 @@ pub fn run(listen_port: String, forward_to: String) -> Result<()> {
         Ok(())
     });
 
-    l.run(done).unwrap();
+    try!(l.run(done));
 
     Ok(())
 }
